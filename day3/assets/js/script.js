a5992912f0c30637299e49b5ae0d32dc1520eff1
Jeremy Santoso <97815661+jeromedesantos12@users.noreply.github.com>
2025-07-08 23:13:15 +0700

    perbaikan z-index

--- day3/assets/js/script.js
+++ day3/assets/js/script.js
@@ -2,6 +2,5 @@ const toggleMenu = document.querySelector(`.menu-toggle`);
 const miniMenu = document.querySelector(`.menu-mini`);
 
 toggleMenu.addEventListener(`click`, () => {
-  console.log(`Jalan`);
   miniMenu.classList.toggle(`active`);
 });
