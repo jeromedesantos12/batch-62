cb9a4bf27b35d9cbb2253678e503029762040f3b
Jeremy Santoso <97815661+jeromedesantos12@users.noreply.github.com>
2025-07-16 12:52:28 +0700

    tambah task5

---
+++ task5/index.js
@@ -0,0 +1,15 @@
+const express = require("express"); // node manggil express
+const app = express(); // manggil fungsi express
+const port = 3000; // port 3000
+
+// http method -> get
+// req: client
+// res: server
+app.get("/", (req, res) => {
+  // route get untuk manggil data
+  res.send("Halo Welt! Freut Mich! Ich bin Jeremy, Wie haisse du?"); // respond dari server
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`); // manggil port
+}); // localhost:3000
