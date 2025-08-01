646c8e857fb825720ee6050f28f575ce604c3d8e
Jeremy Santoso <97815661+jeromedesantos12@users.noreply.github.com>
2025-08-01 23:14:13 +0700

    fitur darkmode selesai

--- task10/src/js/script.js
+++ task10/src/js/script.js
@@ -1,14 +1,15 @@
-// Hamburger
 const hamburger = document.querySelector(`#hamburger`);
 const navMenu = document.querySelector(`#nav-menu`);
 const home = document.querySelector(`#home`);
 const toTop = document.querySelector(`#to-top`);
 
+// Nav menu toggle
 hamburger.addEventListener(`click`, () => {
   hamburger.classList.toggle(`hamburger-active`);
   navMenu.classList.toggle(`hidden`);
 });
 
+// Close nav menu when clicking outside the menu
 window.addEventListener(`click`, (e) => {
   if (e.target !== hamburger && e.target !== navMenu) {
     hamburger.classList.remove(`hamburger-active`);
@@ -16,6 +17,7 @@ window.addEventListener(`click`, (e) => {
   }
 });
 
+// Scroll to top button
 window.onscroll = () => {
   if (window.pageYOffset > home.offsetTop) {
     toTop.classList.remove(`invisible`);
@@ -25,3 +27,15 @@ window.onscroll = () => {
     toTop.classList.add(`opacity-0`);
   }
 };
+
+// Darkmode toggle
+const darkToggle = document.querySelector(`#dark-toggle`);
+const html = document.querySelector(`html`);
+
+darkToggle.addEventListener(`click`, () => {
+  if (darkToggle.checked) {
+    html.classList.add(`dark`);
+  } else {
+    html.classList.remove(`dark`);
+  }
+});
